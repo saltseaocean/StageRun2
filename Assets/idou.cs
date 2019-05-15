8150ca9dc2600245fc85d9f3a119d1b63a43bdbb
onodera_umi <smallocean927@gmail.com>
2019-05-15 09:53:40 +0900

    [add]はじめてのこみっと

---
+++ Assets/idou.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class idou : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			SceneManager.LoadScene ("end2");
+		}
+	}
+}
